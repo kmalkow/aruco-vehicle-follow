f05aabc7d60e157faf397308b1f4075a142e76a2
smarinp <smarin11@icloud.com>
2023-09-03 22:49:37 +0200

    axis system conversion

---
+++ coordinate_conversion_2.py
@@ -0,0 +1,129 @@
+import pymap3d
+import numpy as np
+import sys
+
+sys.path.append("/home/sergio/paparazzi/sw/ext/pprzlink/lib/v2.0/python/")
+
+from ivy.std_api import *
+import pprzlink.ivy
+import pprzlink.messages_xml_map as messages_xml_map
+import pprzlink.message as message       
+
+
+pitch_values = None         # Global variable to store Ivybus received pitch values
+roll_values = None          # Global variable to store Ivybus received pitch values
+yaw_values = None           # Global variable to store Ivybus received pitch values
+
+
+# --------- Bind to Drone Attitude Message --------- # 
+def attitude_callback(ac_id, pprzMsg):
+    global pitch_values
+    global roll_values
+    global yaw_values
+    
+    pitch = pprzMsg['theta']
+    roll  = pprzMsg['phi']
+    yaw   = pprzMsg['psi']
+    pitch_values = pitch
+    roll_values  = roll
+    yaw_values   = yaw
+
+# --------- Get Attitude Values --------- # 
+def get_attitude_values():
+    global pitch_values
+    global roll_values
+    global yaw_values
+    return pitch_values, roll_values, yaw_values
+
+
+                                      # Ivybus INITIALISATION #
+# ------------------------------------------------------------------------------------------------------- #
+# --------- Create Ivy Interface --------- # 
+ivy = pprzlink.ivy.IvyMessagesInterface(agent_name="ArucoMarker", start_ivy=False, ivy_bus="127.255.255.255:2010")
+
+# --------- Start Ivy Interface --------- # 
+ivy.start()
+
+# --------- Subscribe to Ivy Messages --------- # 
+ivy.subscribe(attitude_callback, message.PprzMessage("telemetry", "NPS_RATE_ATTITUDE"))
+
+
+while True:
+        
+    pitch, roll, yaw = get_attitude_values()
+
+    print("pitch: ", pitch)
+    print("roll: ", roll)
+    print("yaw: ", yaw)
+    print("")
+
+
+# # Drone Outputs
+# lat_drone = 1  # Replace with actual latitude of the drone
+# lon_drone = 1  # Replace with actual longitude of the drone
+# alt_drone = 1  # Replace with actual altitude of the drone
+
+# pitch_drone = 1  # Replace with actual pitch of the drone in radians
+# roll_drone = 1   # Replace with actual roll of the drone in radians
+# yaw_drone = 1    # Replace with actual yaw of the drone in radians
+
+# # Local coordinates of the ArUco marker with respect to the drone
+# local_x = 2  # Replace with actual local x-coordinate
+# local_y = 3  # Replace with actual local y-coordinate
+# local_z = 4  # Replace with actual local z-coordinate
+
+# # STEP 1: Convert Drone's Geodetic Coordinates to ECEF
+# ecef_drone = pymap3d.geodetic2ecef(lat_drone, lon_drone, alt_drone)
+
+# # STEP 2: Define Rotation Matrix (Yaw, Pitch, Roll)
+# def rotation_matrix_yaw_pitch_roll(yaw, pitch, roll):
+#     # Yaw (rotation around z-axis)
+#     R_yaw = np.array([
+#         [np.cos(yaw), -np.sin(yaw), 0],
+#         [np.sin(yaw), np.cos(yaw), 0],
+#         [0, 0, 1]
+#     ])
+
+#     # Pitch (rotation around y-axis)
+#     R_pitch = np.array([
+#         [np.cos(pitch), 0, np.sin(pitch)],
+#         [0, 1, 0],
+#         [-np.sin(pitch), 0, np.cos(pitch)]
+#     ])
+
+#     # Roll (rotation around x-axis)
+#     R_roll = np.array([
+#         [1, 0, 0],
+#         [0, np.cos(roll), -np.sin(roll)],
+#         [0, np.sin(roll), np.cos(roll)]
+#     ])
+
+#     R_combined = R_yaw @ R_pitch @ R_roll
+
+#     return R_combined
+
+# # STEP 3: Rotate Local Coordinates to NED Coordinates
+# def local_to_NED(local_x, local_y, local_z, yaw, pitch, roll):
+#     # Create rotation matrix from drone's orientation
+#     R_body_to_NED = np.linalg.inv(rotation_matrix_yaw_pitch_roll(yaw, pitch, roll))
+
+#     # Create a 3x1 vector for local coordinates
+#     local_vector = np.array([[local_x], [local_y], [local_z]])
+
+#     # Rotate local coordinates to NED coordinates
+#     ned_vector = R_body_to_NED @ local_vector
+
+#     # Extract NED coordinates
+#     north, east, down = ned_vector.squeeze()
+
+#     return north, east, down
+
+# # Convert local coordinates to NED coordinates with respect to the drone
+# ned_north, ned_east, ned_down = local_to_NED(local_x, local_y, local_z, yaw_drone, pitch_drone, roll_drone)
+
+# # Print NED coordinates
+# print("NED Coordinates (North, East, Down):", ned_north, ned_east, ned_down)
+
+
+ivy.shutdown()
+
