8a650942047a7e9948c8d3f3861be6bdeb43338e
Christophe De Wagter <dewagter@gmail.com>
2023-09-11 19:30:05 +0200

    KALMAN filter

--- filterV1.py
+++ filterV1.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+#############################################################################
+## Smooth track
+
 y = np.asarray([-31.9311054713794,
 271.783338767474,
 -32.0712612077143,
@@ -46,6 +49,10 @@ y = np.asarray([-31.9311054713794,
 -31.9311054713794,
 271.783338767474])
 
+#y = np.asarray([
+#
+#])
+
 n = len(y)
 y = y.reshape((int(n/2),2))
 n = len(y)
@@ -54,18 +61,102 @@ x = range(0, n)
 tck1 = interpolate.splrep(x, y[:,0], s=0.001, k=3)
 tck2 = interpolate.splrep(x, y[:,1], s=0.001, k=3)
 
+# 10000 = 4m/s
 x_new = np.linspace(min(x), max(x), 10000)
 y_fitx = interpolate.BSpline(*tck1)(x_new)
 y_fity = interpolate.BSpline(*tck2)(x_new)
 
+fit = np.vstack((y_fitx, y_fity)).T
 
-nr = 4015
+start = np.asarray([[-113.6], [67]])
 
-def predict(X):
+
+distances = np.sqrt((fit[:, 0] - start[0])**2 + (fit[:, 1] - start[1])**2)
+closest_index = np.argmin(distances)
+
+print('Start at:',closest_index)
+
+
+nr = closest_index
+
+#############################################################################
+## Kalman filter stuff
+
+dt = 1.0 / 15.0
+
+x = np.asarray([[start[0]],
+                [start[1]],
+                [0],
+                [0]])
+
+A = np.asarray([[1,  0,  dt,  0],
+                [0,  1,  0,   dt],
+                [0,  0,  1,   0],
+                [0,  0,  0,   1]] )
+
+H = np.asarray([[1,  0, 0 ,0],
+                [0,  1, 0 ,0]] )
+
+
+# print('A',A)
+# print('H',H)
+
+K0 = 1e5
+Kp = 1
+Kv = 0.0001
+Kpv = 0
+Km = 1e5
+
+Q = np.asarray([[Kp, 0, Kpv, 0],
+                [0, Kp, 0, Kpv],
+                [Kpv, 0, Kv, 0],
+                [0, Kpv, 0 ,Kv]])
+R = np.asarray([[Km],[Km]])
+P = np.asarray([[K0, 0, 0, 0],
+                [0, K0, 0, 0],
+                [0, 0,  0, 0],
+                [0, 0,  0, 0]])
+
+
+#print('P', P)
+#print('Q', Q)
+#print('R', R)
+
+KP = 0.2
+KV = 0.0001
+
+
+
+def init(N, E, D):
+    global x
+
+    x = np.asarray([[N],
+                [E],
+                [0],
+                [0]])
+    
+    print('X0 set to:', x)
+
+
+def predict(Xunsued):
+    global x
+    global A
+    global P
+    global Q
+
+    # Kalman predict
+    x = A @ x
+    P = ((A @ P) @ A.T) + Q
+
+    return x
+
+
+def route(Xunsued):
     global nr
     global y_fitx
     global y_fity
 
+    # Hack: move along the track blindly
     nr += 1
     if nr >= len(y_fitx):
         nr = 0
@@ -77,8 +168,31 @@ def predict(X):
     return x
 
 
-def correct(X, Z):
-    return Z
+def correct(X_unused, Z):
+    global x
+    global H
+    global R
+    global P
+    global KP
+    global KV
+
+    zk = np.asarray([[Z[0]],
+                     [Z[1]]])
+    #print('z',zk)
+    yk = zk - H@x
+    S = H @ P @ H.T + R
+    #print('S',S)
+    #Si = np.linalg.inv(S)
+    #print('S-1',Si)
+    #K = (P @ H.T) @ Si
+    K = np.asarray([[KP, 0],[0, KP],[KV, 0],[0, KV]])
+    #print('K',K)
+    x = x + (K @ yk)
+    #print('K*yk',K @ yk)
+    P = (np.eye(4) - (K @ H)) @ P
+    #print('P',P)
+
+    return x
 
 
 
